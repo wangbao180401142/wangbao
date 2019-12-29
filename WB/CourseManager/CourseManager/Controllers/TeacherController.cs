cd01450545bf625ceeee1a96c197c2f9a3db7584
123 <123>
2019-12-29 09:12:55 +0800

    12.25

---
+++ WB/CourseManager/CourseManager/Controllers/TeacherController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseManager.Models;
+
+namespace CourseManager.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private courseManagerEntities db = new courseManagerEntities();
+
+        //
+        // GET: /Teacher/
+
+        public ActionResult Index()
+        {
+            return View(db.Teacher.ToList());
+        }
+
+        //
+        // GET: /Teacher/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Teacher teacher = db.Teacher.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        //
+        // GET: /Teacher/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Teacher/Create
+
+        [HttpPost]
+        public ActionResult Create(Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Teacher.Add(teacher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(teacher);
+        }
+
+        //
+        // GET: /Teacher/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Teacher teacher = db.Teacher.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        //
+        // POST: /Teacher/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(teacher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        //
+        // GET: /Teacher/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Teacher teacher = db.Teacher.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        //
+        // POST: /Teacher/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Teacher teacher = db.Teacher.Find(id);
+            db.Teacher.Remove(teacher);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WB/CourseManager/CourseManager/Models/ClassesExtention.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseManager.Models
+{
+    public partial class Classes
+    {
+        [Display(Name="班主任")]
+        public string TeacherName
+        {
+            get {
+                if (!TeacherId.HasValue)
+                {
+                    return "";
+                }
+                courseManagerEntities db = new courseManagerEntities();
+                var teacher = db.Teacher.Where(t => t.Id == TeacherId.Value).FirstOrDefault();
+                if (teacher == null)
+                {
+                    return "";
+                }
+                return teacher.Name;
+            }
+        }
+    }
+}
